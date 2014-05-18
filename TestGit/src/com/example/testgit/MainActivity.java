86ddc33ad9954c97b3a36342e6489f12bca9f067
Winghin2517 <simon.chan2014@gmail.com>
2014-05-18 10:39:20 +0200

    added a private variable String called big.

--- TestGit/src/com/example/testgit/MainActivity.java
+++ TestGit/src/com/example/testgit/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 public class MainActivity extends Activity {
-
+	private String big;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
